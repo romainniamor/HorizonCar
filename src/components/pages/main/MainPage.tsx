202b0f34b76e5c6582ddac97cbbb2e5edb34cf18
romainniamor <romainnavoret@gmail.com>
2024-02-16 11:40:17 +0100

    🚧 F04: MAIN > Add layout

--- src/components/pages/main/MainPage.tsx
+++ src/components/pages/main/MainPage.tsx
@@ -1,4 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme/index";
 
 export default function MainPage() {
   const { userName } = useParams();
@@ -8,9 +10,45 @@ export default function MainPage() {
     navigate("/");
   };
   return (
-    <div>
-      <p>MainPage bienvenue {userName}</p>
-      <button onClick={handleClick}>Se deconnecter</button>
-    </div>
+    <MainPageStyled>
+      <div className="navbar">
+        <p>logo</p>
+        <div className="right-side">
+          <p>MainPage bienvenue {userName}</p>
+          <button onClick={handleClick}>Se deconnecter</button>
+        </div>
+      </div>
+      <div className="main-content"></div>
+    </MainPageStyled>
   );
 }
+
+const MainPageStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.colors.background_white};
+
+  .navbar {
+    width: 100%;
+    height: 60px;
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+
+    .right-side {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+    }
+  }
+
+  .main-content {
+    width: 100%;
+    flex: 1;
+    background-color: pink;
+  }
+`;
---
+++ src/components/pages/main/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { theme } from "../../../theme/index";
+
+type Props = {};
+
+export default function NavBar({}: Props) {
+  return <NavBarStyled>NavBar</NavBarStyled>;
+}
+
+const NavBarStyled = styled.div``;
