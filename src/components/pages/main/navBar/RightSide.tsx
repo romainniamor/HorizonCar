2067d760cd3d5aacd730c2ef54bda2a360c28215
romainniamor <romainnavoret@gmail.com>
2024-02-17 21:12:57 +0100

    ✨ F05: NAVBAR > Right side add link hovered

--- src/components/pages/main/navBar/RightSide.tsx
+++ src/components/pages/main/navBar/RightSide.tsx
@@ -1,22 +1,73 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {
+  FaRegUser,
+  FaRegHeart,
+  FaUserAlt,
+  FaHeart,
+  FaBell,
+  FaRegBell,
+} from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { theme } from "../../../../theme/index";
+import { useState } from "react";
 
-type RightSideProps = {
-  userName: string;
-};
+export default function RightSide() {
+  const [hoveredIcons, setHoveredIcons] = useState([]);
 
-export default function RightSide({ userName }: RightSideProps) {
+  const handleHover = (index) => {
+    setHoveredIcons((prevHoveredIcons) => {
+      const updatedHoveredIcons = [...prevHoveredIcons];
+      updatedHoveredIcons[index] = !updatedHoveredIcons[index];
+      return updatedHoveredIcons;
+    });
+  };
+
+  console.log(hoveredIcons);
+
+  const icons = [
+    { icon: <FaRegHeart />, altIcon: <FaHeart /> },
+    { icon: <FaRegBell />, altIcon: <FaBell /> },
+    { icon: <MdOutlineShoppingCart />, altIcon: <MdOutlineShoppingCart /> },
+    { icon: <FaRegUser />, altIcon: <FaUserAlt /> },
+  ];
+
   return (
     <RightSideStyled>
-      <span>Bonjour</span>
-      <span>{userName}</span>
-      <Link to={"/"}>Se deconnecter</Link>
+      {icons.map((item, index) => (
+        <div
+          key={index}
+          className={`nav-icon ${hoveredIcons[index] ? "hovered" : ""}`}
+          onMouseEnter={() => handleHover(index)}
+          onMouseLeave={() => handleHover(index)}
+        >
+          {hoveredIcons[index] ? item.altIcon : item.icon}
+        </div>
+      ))}
     </RightSideStyled>
   );
 }
 
 const RightSideStyled = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
-  gap: 20px;
+  flex: 1;
+  font-size: ${theme.fonts.P3};
+  background-color: transparent;
+  height: 100%;
+  margin-right: 20px;
+
+  .nav-icon {
+    color: ${theme.colors.white};
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    cursor: not-allowed;
+
+    &:hover {
+      color: ${theme.colors.primary};
+      background-color: ${theme.colors.white};
+    }
+  }
 `;
