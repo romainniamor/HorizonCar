af8d6aaabe6008eaf49ea66782349d9666c42a8e
romainniamor <romainnavoret@gmail.com>
2024-02-19 23:45:29 +0100

    🚧 F08: MAIN > Add textInput

--- src/components/pages/main/mainContent/MainContent.tsx
+++ src/components/pages/main/mainContent/MainContent.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import Hero from "./hero/Hero";
 import { theme } from "../../../../theme/index";
-import { scrollToTop } from "../../../../utils/window";
+import IconButton from "../../../reusable/IconButton";
+
+import { FiSearch } from "react-icons/fi";
 
 type Props = {};
 
@@ -9,12 +11,51 @@ export default function MainContent({}: Props) {
   return (
     <MainContentStyled>
       <Hero />
+      <form>
+        <div className="text-input">
+          <input
+            type="text"
+            placeholder="Recherchez par marque, modèle ou mot-clé (par exemple, Renault Clio"
+            className="input"
+          />
+          <IconButton icon={<FiSearch />} />
+        </div>
+      </form>
     </MainContentStyled>
   );
 }
 
 const MainContentStyled = styled.div`
   width: 100%;
-  flex: 1;
+
   background-color: ${theme.colors.background_white};
+
+  .text-input {
+    border-radius: ${theme.borderRadius.round};
+    display: flex;
+    align-items: center;
+    padding: 8px 15px;
+    color: ${theme.colors.greyLight};
+    font-size: ${theme.fonts.sm};
+    border: 1px solid ${theme.colors.greyLight};
+    background-color: ${theme.colors.white};
+    box-shadow: ${theme.shadows.subtle};
+    &:hover {
+      border: 1.5px solid ${theme.colors.primary};
+    }
+
+    input {
+      border: none;
+      width: 100%;
+      background: white;
+      color: ${theme.colors.dark};
+      font-size: ${theme.fonts.P0};
+      &::placeholder {
+        color: ${theme.colors.greyMedium};
+      }
+      &:focus {
+        outline: 0px;
+      }
+    }
+  }
 `;
