a0ed2b59305544dd070b337a16456ea255553051
romainniamor <romainnavoret@gmail.com>
2024-02-19 16:14:26 +0100

    ✨ F06: HERO > Add casiono effect transition

--- src/components/pages/main/mainContent/hero/Hero.tsx
+++ src/components/pages/main/mainContent/hero/Hero.tsx
@@ -2,63 +2,86 @@ import styled from "styled-components";
 import { theme } from "../../../../../theme/index";
 import { FaMousePointer, FaExchangeAlt, FaCar } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export default function Hero() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const messages = [
+  const messagesData = [
     { icon: <FaCar />, label: "rechercher des vehicules" },
     { icon: <FaMousePointer />, label: "sélectionner deux véhicules" },
     { icon: <FaExchangeAlt />, label: "comparer les véhicules" },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((index) => (index + 1) % messages.length);
-    }, 3500);
+      setCurrentIndex((index) => (index + 1) % messagesData.length);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <HeroStyled>
-      <div className="hero-item">
-        <div className="icon">{messages[currentIndex].icon}</div>
-        <span className="message">{messages[currentIndex].label}</span>
-      </div>
-    </HeroStyled>
+    <TransitionGroup component={HeroStyled}>
+      <CSSTransition
+        timeout={3000}
+        classNames="item-animated"
+        key={currentIndex}
+      >
+        <div className="hero-item">
+          <div className="icon">{messagesData[currentIndex].icon}</div>
+          <span className="message">{messagesData[currentIndex].label}</span>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
 const HeroStyled = styled.div`
-  width: 100%;
   height: 50px;
+  position: relative;
   background-color: ${theme.colors.white};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 2px solid blue;
+  overflow: hidden;
+
   .hero-item {
+    position: absolute;
+    right: 0;
+    bottom: 0;
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: ${theme.fonts.S};
     font-weight: ${theme.weights.medium};
     gap: ${theme.spacing.xs};
-    line-height: 1.5;
-    overflow: hidden;
-    background: red;
+    height: 100%;
+    width: 100%;
+  }
 
-    transition: all 0.5s ease;
+  .icon {
+    display: flex;
+    color: ${theme.colors.primary};
+  }
 
-    .icon {
-      display: flex;
-      color: ${theme.colors.primary};
-    }
+  .message {
+    color: ${theme.colors.secondary};
+    text-transform: capitalize;
+  }
 
-    .message {
-      color: ${theme.colors.secondary};
-      text-transform: capitalize;
-    }
+  .item-animated-enter {
+    transform: translateY(100%);
+  }
+
+  .item-animated-enter-active {
+    transform: translateY(0%);
+    transition: transform 0.5s ease-in-out;
+  }
+
+  .item-animated-exit {
+    transform: translateY(0%);
+  }
+
+  .item-animated-exit-active {
+    transform: translateY(-100%);
+    transition: all 0.5s ease-in-out;
   }
 `;
