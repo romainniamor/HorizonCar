58816ae81a0b412c61544ac7cf992ba57b00bbf9
romainniamor <romainnavoret@gmail.com>
2024-02-26 16:43:11 +0100

    🚧 F12: MainContent > Filter carList & make reusable MainForm

--- src/components/pages/main/mainContent/carList/CarList.tsx
+++ src/components/pages/main/mainContent/carList/CarList.tsx
@@ -11,14 +11,54 @@ type Props = {};
 
 export default function CarList({}: Props) {
   //state
-  const [cars] = useState(FAKEPARK);
+  const [cars, setCars] = useState(FAKEPARK);
   const { handleAddCartoSelection, isCarSelected } = useContext(MainContext);
 
+  const [inputValue, setInputValue] = useState("");
+
   //comportement
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const copyCars = [...cars];
+    console.log("copyCars", copyCars);
+    const updatedCars = copyCars.filter((car) =>
+      car.modele.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setCars(updatedCars);
+    addFilter(inputValue);
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addFilter = (filter) => {
+    const newFilter = [...filter, filter];
+    setFilter(newFilter);
+  };
+
+  const resetFilter = () => {
+    setCars(FAKEPARK);
+    setInputValue("");
+  };
 
   //affichage
   return (
     <CarListStyled>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={inputValue} />
+      </form>
+      <div className="filter">
+        <span className="results">
+          {cars.length}{" "}
+          {cars.length === 1 ? <span>Résultat</span> : <span>Résultats</span>}
+        </span>
+        <p onClick={resetFilter}>Effacer le filtre</p>
+      </div>
+      {!cars.length && <p>Aucun résultat pour '{inputValue}'</p>}
+
       {cars.map((car) => {
         return (
           <Car {...car} key={car.id}>
@@ -38,5 +78,5 @@ const CarListStyled = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(480px, 1fr));
   width: 100%;
   grid-gap: 15px;
-  padding: 0 30px 170px 30px;
+  padding: 0px 30px 170px 30px;
 `;
--- src/components/pages/main/mainContent/form/MainForm.tsx
+++ src/components/pages/main/mainContent/form/MainForm.tsx
@@ -1,18 +1,39 @@
 import IconButton from "../../../../reusable/buttons/IconButton";
 import { FiSearch } from "react-icons/fi";
 import styled from "styled-components";
+import { RxCross2 } from "react-icons/rx";
 import { theme } from "../../../../../theme/index";
-type Props = {};
 
-export default function MainForm({}: Props) {
+type Props = {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  onDelete: () => void;
+};
+
+export default function MainForm({
+  onSubmit,
+  onChange,
+  value,
+  onDelete,
+}: Props) {
   return (
-    <FormStyled>
+    <FormStyled onSubmit={onSubmit}>
       <div className="text-input">
         <input
           type="text"
           placeholder="Recherchez par marque, modèle ou mot-clé (par exemple, Renault Clio"
           className="input"
+          onChange={onChange}
+          value={value}
         />
+
+        {value && (
+          <div className="delete-button" onClick={onDelete}>
+            <RxCross2 />
+          </div>
+        )}
+
         <IconButton icon={<FiSearch />} />
       </div>
     </FormStyled>
@@ -29,6 +50,7 @@ const FormStyled = styled.form`
     border-radius: ${theme.borderRadius.round};
     display: flex;
     align-items: center;
+    gap: ${theme.spacing.xs};
     padding: 8px 15px;
     color: ${theme.colors.greyLight};
     font-size: ${theme.fonts.sm};
@@ -46,12 +68,18 @@ const FormStyled = styled.form`
       background: white;
       color: ${theme.colors.dark};
       font-size: ${theme.fonts.P0};
+
       &::placeholder {
         color: ${theme.colors.greyMedium};
       }
       &:focus {
         outline: 0px;
       }
+    }
+    .delete-button {
+      display: flex;
+      color: ${theme.colors.secondary};
+      font-size: ${theme.fonts.P2};
     }
   }
 `;
