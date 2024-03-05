06c5190e20286225955c6b80275daf5754a17bc8
romainniamor <romainnavoret@gmail.com>
2024-03-05 14:43:16 +0100

    🚧 💄  F15: CarPage > Add layout to carPage

--- src/components/pages/car/CarPage.tsx
+++ src/components/pages/car/CarPage.tsx
@@ -1,13 +1,27 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import PrimaryButton from "../../reusable/buttons/PrimaryButton";
+import { useNavigate } from "react-router-dom";
 
 export default function CarPage() {
   const selectedCar = JSON.parse(localStorage.getItem("selectedCar"));
-  console.log(selectedCar);
+  const navigate = useNavigate();
 
   return (
     <CarPageStyled>
-      <h2>CarPage de la voiture {selectedCar.modele} </h2>
+      <div className="content">
+        <h2>Connexion ou Inscription</h2>
+        <span>Pour continuer et voir les information de la voiture </span>
+        <span className="modele">{selectedCar.modele}</span>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input name="email" type="text" />
+        </form>
+        <PrimaryButton
+          label="Revenir à l'accueil"
+          onClick={() => navigate("/")}
+        />
+      </div>
     </CarPageStyled>
   );
 }
@@ -16,6 +30,12 @@ const CarPageStyled = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   background-color: ${theme.colors.background_white};
+  padding: 20px;
+  .content {
+    background: ${theme.colors.white};
+    padding: 20px;
+  }
 `;
